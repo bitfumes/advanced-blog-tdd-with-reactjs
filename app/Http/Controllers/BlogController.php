de588e4027aff353388b05f246b59f3e778c06a6
sarthaksavvy <sarthak@bitfumes.com>
2019-04-20 13:18:41 +0530

    blog is saved with green test

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Blog;
 
 class BlogController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-        # code...
+        Blog::create($request->all());
     }
 }
